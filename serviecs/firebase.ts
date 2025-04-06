aaaab39e67a164b034e723724425f768f5130daf
IgnaBV <ignabenavidesvargas@gmail.com>
2025-04-06 15:18:38 -0400

    Primer commit

---
+++ serviecs/firebase.ts
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDd6h9Shp4FT_9TmZv6YFAhD6e6FDdJad8",
+  authDomain: "filaexpress-1d5bf.firebaseapp.com",
+  projectId: "filaexpress-1d5bf",
+  storageBucket: "filaexpress-1d5bf.firebasestorage.app",
+  messagingSenderId: "553705646225",
+  appId: "1:553705646225:web:c849b6019c73055146b730"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
